6c4606f70adb00404b1b798b1366ff6f0b24138d
Fatoma Farooqi <fatomafarooqi@Fatomas-MacBook-Air.local>
2024-02-04 23:28:27 +0300

    first commit

---
+++ index.js
@@ -0,0 +1,20 @@
+var name='fatma'
+var age=18
+var isStudint=true
+console.log(name)
+console.log(age)
+console.log(isStudint)
+var variableToCheck=5
+console.log(typeof variableToCheck)
+var name='taha'
+var greeting='hello'
+console.log(greeting+' '+name)
+
+var a=2
+var b=4
+console.log(a>b)
+console.log(a<b)
+console.log(a==b)
+console.log(a>=b)
+console.log(a<=b)
+console.log(a!=b)
